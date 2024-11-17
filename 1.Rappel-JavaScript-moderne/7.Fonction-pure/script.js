88c96c19e50ba2463d755746c4b5353d0750e2fe
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-17 16:21:04 +0100

    Les fonctions pures

--- 1.Rappel-JavaScript-moderne/7.Fonction-pure/script.js
+++ 1.Rappel-JavaScript-moderne/7.Fonction-pure/script.js
@@ -1,10 +1,30 @@
-/* 
+/*
     1. Fonction pure.
 
     Une fonction pure a deux caractéristiques :
     1. Elle retourne toujours la même chose en fonction des mêmes arguments fournis.
-    2. Elle n'utilise aucune valeurm mutable et n'a pas d'effets secondaires en dehors de son bloc.
+    2. Elle n'utilise aucune valeurm mutable et n'a pas d'effets secondaires en
+    dehors de son bloc.
 
 */
 
+function add(a, b) {
+  return a + b;
+}
+// console.log(add(10, 10)); // 20
 
+// // let VAT = 20;
+// // VAT = 17;
+// const FRVAT = { value: 20 };
+// FRVAT.value = 17;
+// function getFinalPrice(price, VAT) {
+//   return (price * (100 + FRVAT.value)) / 100;
+// }
+// console.log(getFinalPrice(100)); // 117
+
+function getFinalPrice(price, VAT) {
+  return (price * (100 + VAT)) / 100;
+}
+console.log(getFinalPrice(100, 20)); // 120
+console.log(getFinalPrice(100, 5)); // 105
+console.log(getFinalPrice(100, 27)); // 127
