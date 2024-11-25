daae7cfdff49868bb9aed3c951455c805defb99e
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-25 17:02:32 +0100

    Utiliser les modules es6

---
+++ 1.Rappel-JavaScript-moderne/12.Modules-es6/script.js
@@ -0,0 +1,9 @@
+// import { age, userName } from "./module.js";
+// import foo from "./module.js";
+// import * as Utils from "./module.js";
+import "./module.js";
+
+// foo(); // Hello from module
+// console.log(age, userName); // 45 "Giovanni"
+
+// console.log(Utils);
