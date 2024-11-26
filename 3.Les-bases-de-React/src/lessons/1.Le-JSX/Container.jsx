805b6c3ef82749e363be6c5e2e3cd963d7e93324
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-26 15:08:32 +0100

    Le JSX

---
+++ 3.Les-bases-de-React/src/lessons/1.Le-JSX/Container.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Container() {
+  return (
+    <div>
+      <p></p>
+      <ul className="list">
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Container;
