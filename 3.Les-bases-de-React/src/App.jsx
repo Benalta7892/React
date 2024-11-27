acd8250c24ebc095cb9c2cf356ce4aaaa78d12af
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-27 15:29:22 +0100

    Utiliser du CSS

--- 3.Les-bases-de-React/src/App.jsx
+++ 3.Les-bases-de-React/src/App.jsx
@@ -1,9 +1,9 @@
-import Container from "./lessons/5.Les-props/Container";
+import Container from "./lessons/6.Utiliser-du-CSS/Container";
 
 function App() {
   return (
     <div className="main-content">
-      <h1>Cours React</h1>
+      <p>Cours React</p>
       <Container />
     </div>
   );
--- 3.Les-bases-de-React/src/lessons/6.Utiliser-du-CSS/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/6.Utiliser-du-CSS/Container.jsx
@@ -1,8 +1,18 @@
+import "./Container.css";
+
 export default function Container() {
+  const bgColor = "#333";
 
   return (
+    // CSS en ligne
     <div>
-      <h1>Utiliser du CSS</h1>
+      <h1 style={{ color: "crimson", backgroundColor: bgColor }}>Utiliser du CSS</h1>
+
+      <p className="txt">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem quibusdam debitis ea, pariatur commodi
+        architecto! Velit vero et blanditiis ex. Incidunt laborum voluptates sit accusantium qui necessitatibus eveniet
+        reiciendis quia!
+      </p>
     </div>
-  )
+  );
 }
