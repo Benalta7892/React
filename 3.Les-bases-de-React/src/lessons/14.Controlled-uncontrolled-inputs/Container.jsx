311c61599581b56e70ceab42ae3542fd77f4d939
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-28 15:07:08 +0100

    Les inputs controlles

--- 3.Les-bases-de-React/src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
@@ -1,10 +1,23 @@
-import "./Container.css"
+import { useState, useRef } from "react";
+import "./Container.css";
 
 export default function Container() {
+  const [state, setState] = useState("");
+
+  function handleInput(e) {
+    setState(e.target.value);
+  }
 
   return (
     <div>
       <h1>Composants "controlled" et "uncontrolled"</h1>
+      <form>
+        <label htmlFor="pseudo">Votre pseudo</label>
+        <input onChange={handleInput} type="text" id="pseudo" value={state} />
+        {!state && <p>Veuillez remplir cet input</p>}
+        <button>Envoyer</button>
+        <p>Votre pseudo : {state}</p>
+      </form>
     </div>
-  )
+  );
 }
