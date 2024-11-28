22d7673269ee996b2079b045a13dfc47ae78fb10
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-28 15:15:11 +0100

    Les Inputs incontrolles

--- 3.Les-bases-de-React/src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
@@ -8,13 +8,27 @@ export default function Container() {
     setState(e.target.value);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(state);
+
+    console.log(emailRef.current.value);
+  }
+
+  const emailRef = useRef();
+
   return (
     <div>
       <h1>Composants "controlled" et "uncontrolled"</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="pseudo">Votre pseudo</label>
         <input onChange={handleInput} type="text" id="pseudo" value={state} />
-        {!state && <p>Veuillez remplir cet input</p>}
+        {/* {!state && <p>Veuillez remplir cet input</p>} */}
+
+        <label htmlFor="email">Votre email</label>
+        <input ref={emailRef} type="email" id="email" />
+
         <button>Envoyer</button>
         <p>Votre pseudo : {state}</p>
       </form>
