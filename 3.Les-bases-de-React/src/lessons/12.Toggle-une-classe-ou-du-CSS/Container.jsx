366845694407e2d339ec201fdc1046fb25acc6a2
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-28 14:36:28 +0100

    Toggle classe

--- 3.Les-bases-de-React/src/lessons/12.Toggle-une-classe-ou-du-CSS/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/12.Toggle-une-classe-ou-du-CSS/Container.jsx
@@ -1,9 +1,18 @@
-import "./Container.css"
+import "./Container.css";
+import { useState } from "react";
 
 export default function Container() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div>
-      <h1>Toggle une classe</h1>
+    <div className={`full-container ${darkMode && "dark-mode"}`}>
+      <h1>Bienvenue sur NotesMania</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id fugit consectetur, assumenda nobis at magni ipsam,
+        eius nulla nam obcaecati rem consequuntur cumque minima deleniti reiciendis, laborum ex minus iure.
+      </p>
+
+      <button onClick={() => setDarkMode(!darkMode)}>Activer le {darkMode ? "Light Mode" : "Dark mode"}</button>
     </div>
-  )
+  );
 }
