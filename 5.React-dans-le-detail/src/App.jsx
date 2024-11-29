cb937223b5b82c5e96924cb9b1502fa2a21cb3cb
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-29 11:53:24 +0100

    Les fragments

---
+++ 5.React-dans-le-detail/src/App.jsx
@@ -0,0 +1,11 @@
+import Container from "./lessons/1.Les-fragments/Container";
+
+function App() {
+  return (
+    <div className="main-content">
+      <Container />
+    </div>
+  );
+}
+
+export default App;
