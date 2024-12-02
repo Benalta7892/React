7140edc0ec478bfa5b0011d6f775f6388f85f27a
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-02 16:24:13 +0100

    Appeler une API

--- 5.React-dans-le-detail/src/App.jsx
+++ 5.React-dans-le-detail/src/App.jsx
@@ -1,3 +1,7 @@
+// Une API est un ensemble de fonctions et de procédures qui permettent à des applications de communiquer entre elles.
+// Par exemple, une API peut permettre à une application de récupérer des données stockées sur un serveur distant.
+// Dans ce chapitre, nous allons apprendre à appeler une API à partir d'une application React.
+
 import Container from "./lessons/6.Appeler-une-API/Container";
 
 function App() {
