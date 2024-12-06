d78ea921f0ae7fdae7822e17eec8e64cd4057f2d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-06 10:55:59 +0100

    Utiliser setInterval

--- 5.React-dans-le-detail/src/lessons/8.Utiliser-setInterval/Container.jsx
+++ 5.React-dans-le-detail/src/lessons/8.Utiliser-setInterval/Container.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react";
+
 export default function Container() {
-  
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setCount(count + 1);
+      // console.log("hello");
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, [count]);
+
+  // setInterval(() => {
+  //   setCount(count + 1);
+  // }, 1000);
+
   return (
     <div>
-      <h1>Valeur du compteur : </h1>
+      <h1>Valeur du compteur : {count}</h1>
     </div>
-  )
+  );
 }
