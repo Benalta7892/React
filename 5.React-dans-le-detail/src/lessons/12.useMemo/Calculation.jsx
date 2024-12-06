5205e2e1a0849c5c02dbc1d3402c84174edfcf72
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-06 14:03:07 +0100

    Le hook useMemo

---
+++ 5.React-dans-le-detail/src/lessons/12.useMemo/Calculation.jsx
@@ -0,0 +1,21 @@
+import { useState, useMemo } from "react";
+
+export default function Calculation() {
+  const [temperature, setTemperature] = useState("");
+
+  function celsiusToFarenheit(degree) {
+    return degree ? degree * 1.8 + 32 : "Entrez une valeur";
+  }
+
+  const expensiveCalculation = celsiusToFarenheit(temperature);
+  console.log("RENDER");
+
+  return (
+    <div>
+      <input type="number" value={temperature} onChange={(e) => setTemperature(e.target.value)} />
+      <h1>
+        Valeur de {temperature} degrés en Farenheit : {expensiveCalculation}
+      </h1>
+    </div>
+  );
+}
