e4ea7233bd582b03f781defe811086273e318ea0
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-06 13:14:33 +0100

    Comprendre les props children

---
+++ 5.React-dans-le-detail/src/lessons/10.Comprendre-props-children/Child.jsx
@@ -0,0 +1,5 @@
+export default function Child(props) {
+  console.log(props);
+
+  return <div>{props.children}</div>;
+}
--- 5.React-dans-le-detail/src/lessons/10.Comprendre-props-children/Container.jsx
+++ 5.React-dans-le-detail/src/lessons/10.Comprendre-props-children/Container.jsx
@@ -1,9 +1,26 @@
-
+import Child from "./Child";
 export default function Container() {
   return (
     <div>
       <h1>Props children</h1>
-
+      {/* <Child tkt={"test"} /> */}
+      <Child>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit dolores aspernatur placeat accusamus,
+          corrupti, veritatis sapiente possimus commodi facilis porro temporibus totam laudantium fuga aliquam delectus
+          error ullam cupiditate consectetur!
+        </p>
+      </Child>
+      <Child>
+        <button>Valider</button>
+      </Child>
+      <Child>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </Child>
     </div>
-  )
+  );
 }
