75188b4b6f4d40f7be0aa9e14d77f0258876f865
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-07 13:48:51 +0100

    Coder une fenetre modale

--- 6.Creer-des-composants-courants/src/App.jsx
+++ 6.Creer-des-composants-courants/src/App.jsx
@@ -1,10 +1,11 @@
-function App() {
+import ModalButton from "./lessons/1.Creer-une-fenetre-modale-avec-un-portal/ModalButton";
 
+function App() {
   return (
     <div className="min-h-screen bg-slate-700 pt-10">
-
+      <ModalButton />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
