cd8365125a8d51403f8662a76f86a739f10e00c8
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-07 14:24:14 +0100

    Coder une navbar responsive

--- 6.Creer-des-composants-courants/src/App.jsx
+++ 6.Creer-des-composants-courants/src/App.jsx
@@ -1,9 +1,9 @@
-import Tabs from "./lessons/2.Coder-des-onglets/Tabs";
+import NavBar from "./lessons/3.Coder-une-navbar/NavBar";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-700 pt-10">
-      <Tabs />
+      <NavBar />
     </div>
   );
 }
