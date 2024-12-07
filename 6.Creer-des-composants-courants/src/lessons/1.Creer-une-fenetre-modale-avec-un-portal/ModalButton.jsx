e5f7eb92ba06275dfc1bf60a8e55893c8416af0a
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-07 13:57:20 +0100

    Coder une fenetre modale

--- 6.Creer-des-composants-courants/src/lessons/1.Creer-une-fenetre-modale-avec-un-portal/ModalButton.jsx
+++ 6.Creer-des-composants-courants/src/lessons/1.Creer-une-fenetre-modale-avec-un-portal/ModalButton.jsx
@@ -3,12 +3,14 @@ import { createPortal } from "react-dom";
 import ModalContent from "./ModalContent";
 
 export default function ModalButton() {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button className="block mx-auto bg-slate-200 text-slate-900 p-2 rounded">Open the modal</button>
-      {showModal && createPortal(<ModalContent />, document.body)}
+      <button onClick={() => setShowModal(true)} className="block mx-auto bg-slate-200 text-slate-900 p-2 rounded">
+        Open the modal
+      </button>
+      {showModal && createPortal(<ModalContent closeModal={() => setShowModal(false)} />, document.body)}
     </>
   );
 }
--- 6.Creer-des-composants-courants/src/lessons/1.Creer-une-fenetre-modale-avec-un-portal/ModalContent.jsx
+++ 6.Creer-des-composants-courants/src/lessons/1.Creer-une-fenetre-modale-avec-un-portal/ModalContent.jsx
@@ -1,3 +1,9 @@
-export default function ModalContent() {
-  return <div>ModalContent</div>;
+export default function ModalContent({ closeModal }) {
+  return (
+    <div onClick={closeModal} className="fixed inset-0 bg-slate-800/75 flex items-center justify-center">
+      <div onClick={(e) => e.stopPropagation()} className="bg-slate-300 text-slate-900 p-10 rounded relative mb-[10vh]">
+        <p>Here is the content</p>
+      </div>
+    </div>
+  );
 }
