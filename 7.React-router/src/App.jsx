edda7c59c3bf4a43887a350da6b500563745fcd8
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-10 16:22:19 +0100

    Creer des routes imbriquees

--- 7.React-router/src/App.jsx
+++ 7.React-router/src/App.jsx
@@ -4,6 +4,10 @@ import Contact from "./components/Contact";
 import UserProfile from "./components/UserProfile";
 import NotFound from "./components/NotFound";
 import Navbar from "./components/Navbar";
+import Analytics from "./pages/Analytics/Analytics";
+import Development from "./pages/Analytics/Development";
+import CyberSecurity from "./pages/Analytics/CyberSecurity";
+import UIUX from "./pages/Analytics/UIUX";
 
 function App() {
   return (
@@ -14,6 +18,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/profile/:id" element={<UserProfile />} />
+          <Route path="/analytics" element={<Analytics />}>
+            <Route path="/analytics/development" element={<Development />} />
+            <Route path="/analytics/cybersecurity" element={<CyberSecurity />} />
+            <Route path="/analytics/uiux" element={<UIUX />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ 7.React-router/src/pages/Analytics/UIUX.jsx
@@ -0,0 +1,3 @@
+export default function UIUX() {
+  return <div>UIUX</div>;
+}
