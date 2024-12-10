686f28b25d33ca2c88e9f1b15f2ad5d9ef59612d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-10 15:36:05 +0100

    Creer une page 404

--- 7.React-router/src/App.jsx
+++ 7.React-router/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import UserProfile from "./components/UserProfile";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/profile/:id" element={<UserProfile />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ 7.React-router/src/components/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div>
+      <p className="text-4xl font-semibold text-center pt-10">Page not found 🤖</p>
+    </div>
+  );
+}
