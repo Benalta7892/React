67448518d6d9165f15db96f1e64d6ec044e4874d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-10 15:28:17 +0100

    Mettre en place React Router

--- 7.React-router/src/App.jsx
+++ 7.React-router/src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+
 function App() {
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 7.React-router/src/components/Contact.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <div>
+      <p className="text-4xl font-semibold text-center pt-10">Contact us.</p>
+    </div>
+  );
+}
