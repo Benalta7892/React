8ee63a136bf61112e7f0a68cf1a58deb3af1b4ce
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-10 17:05:58 +0100

    Creation du contexte

--- 8.Api-de-contexte/src/components/ToggleThemeButton/ToggleThemeButton.jsx
+++ 8.Api-de-contexte/src/components/ToggleThemeButton/ToggleThemeButton.jsx
@@ -1,11 +1,16 @@
 import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
 import "./ToggleThemeButton.css";
+import { ThemeContext } from "../../context/ThemeProvider";
+import { useContext } from "react";
 
-export default function ToggleTheme() {
+export default function ToggleThemeButton() {
+  const { toggleTheme, darkMode } = useContext(ThemeContext);
+
   return (
-    <button className="toggle-theme-btn">
-      <span>Go Dark</span>
-      <img src={sun} alt="toggle icon" />
+    <button onClick={toggleTheme} className="toggle-theme-btn">
+      <span>Go {darkMode ? "Light" : "Dark"}</span>
+      <img src={darkMode ? sun : moon} alt="toggle icon" />
     </button>
   );
 }
