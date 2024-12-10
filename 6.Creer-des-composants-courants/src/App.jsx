e7c4b2bc91246028e150d3283bff95beea812f24
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-10 11:09:09 +0100

    Mise en place d'un systeme de validation

--- 6.Creer-des-composants-courants/src/App.jsx
+++ 6.Creer-des-composants-courants/src/App.jsx
@@ -1,9 +1,9 @@
-import NavBar from "./lessons/4.Validation/Validation.jsx";
+import Validation from "./lessons/4.Validation/Validation.jsx";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-700 pt-10">
-      <NavBar />
+      <Validation />
     </div>
   );
 }
