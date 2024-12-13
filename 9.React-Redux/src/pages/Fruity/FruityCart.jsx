9675b4b1bd7c64208475b9fc76e7ae2cb981dbd2
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-13 10:49:53 +0100

    Afficher les fruits selectionnes

--- 9.React-Redux/src/pages/Fruity/FruityCart.jsx
+++ 9.React-Redux/src/pages/Fruity/FruityCart.jsx
@@ -7,7 +7,15 @@ export default function FruityCart() {
   return (
     <div className="bg-slate-100 rounded">
       <p className="text-xl p-5 border-b border-slate-400">Your FruityCart</p>
-      <ul></ul>
+      <ul>
+        {fruityCart.cart.map((fruit) => (
+          <li className="px-5 py-2 text-xl" key={fruit.id}>
+            <span className="font-semibold mr-2">{fruit.quantity}</span>
+            <span>{fruit.name}</span>
+          </li>
+        ))}
+        {fruityCart.cart.length === 0 && <li className="px-5 py-2 text-xl font-semibold">... Grab one fruit !</li>}
+      </ul>
       <p className="text-xl p-5 border-t border-slate-400">Total Price :</p>
     </div>
   );
