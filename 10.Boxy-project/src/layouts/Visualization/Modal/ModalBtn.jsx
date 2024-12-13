94d7406be2855495893799781822978200958a79
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-12-13 17:36:30 +0100

    Ajouter la visualisation

---
+++ 10.Boxy-project/src/layouts/Visualization/Modal/ModalBtn.jsx
@@ -0,0 +1,9 @@
+export default function ModalBtn() {
+  return (
+    <div
+      className="mx-auto mt-2 py-1 px-3 cursor-pointer text-sm rounded
+    bg-blue-600 text-white hover:bg-blue-700">
+      Get the code
+    </div>
+  );
+}
---
+++ 10.Boxy-project/src/layouts/Visualization/Modal/ModalResult.jsx
@@ -0,0 +1,3 @@
+export default function ModalResult() {
+  return <div>ModalResult</div>;
+}
